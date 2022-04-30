b8045ad706b24f57cdee9ae832c3979625f1df05
Sai Main <saimain.venuslab@gmail.com>
2022-04-30 12:41:16 +0630

    change number format

--- index.php
+++ index.php
@@ -2,20 +2,20 @@
 
 header('Content-type: application/json');
 
-$interval = 60 * 5;
+$interval = 60 * 10;
 
 // the random numbers will be change every 5 seconds
 // change (5) to the second you want to set timer for refresh random numbers
 
 srand(floor(time() / $interval * 60));
-$num_one =  rand(1111, 9999);
-$num_two =  rand(1111, 9999);
+$num_one =  rand(111, 999);
+$num_two =  rand(111, 999);
 
 $return_data = [
     'num_one' => '1.' . $num_one,
     'num_two' => '3.' . $num_two,
-    'num_one_f' => substr($num_one, -1),
-    'num_two_f' => substr($num_two, -1)
+    'num_one_f' => (string) $num_one,
+    'num_two_f' => substr($num_one, -1) . substr($num_two, -1)
 ];
 
 
